ecedba4f084caa678ec00668296ad2068f914a64
tonilatorrec <55156031+tonilatorrec@users.noreply.github.com>
2019-10-13 00:34:57 +0200

    added flask.flash

--- hwh.py
+++ hwh.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, flash, redirect, url_for, request, render_template
 import time
 import datetime
 import serial
